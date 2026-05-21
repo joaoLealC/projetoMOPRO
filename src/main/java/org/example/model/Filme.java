217dfd99585905107fa368e8a4ffd89282b89073
beatrizdsp <1250780@isep.ipp.pt>
2026-05-21 16:19:33 +0100

    teste

--- src/main/java/org/example/model/Filme.java
+++ src/main/java/org/example/model/Filme.java
@@ -1,3 +1,39 @@
 package org.example.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Filme extends Recurso implements MarcavelComoVisto {
+    private int duracaoMinutos;
+    private List<Ator> atores;
+    private boolean visto;
+
+    public Filme(String titulo, int ano, int duracaoMinutos) {
+        super(titulo, ano);
+        this.duracaoMinutos = duracaoMinutos;
+        this.atores = new ArrayList<>();
+        this.visto = false;
     }
+
+    public void adicionarAtor(Ator ator) {
+        if (!atores.contains(ator)) {
+            atores.add(ator);
+        }
+    }
+
+    // Resolve os erros da interface MarcavelComoVisto
+    @Override
+    public void marcarComoVisto() {
+        this.visto = true;
+    }
+
+    @Override
+    public boolean estaVisto() {
+        return visto;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + super.toString() + " - " + duracaoMinutos + " min | Visto: " + (visto ? "Sim" : "Não");
+    }
+}
