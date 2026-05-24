14551b0af70343d0798ab6bf498d65863c438d40
gooncalo <1250982@isep.ipp.pt>
2026-05-24 18:53:16 +0100

    teste final

--- src/main/java/org/example/model/UtilizadorRegistado.java
+++ src/main/java/org/example/model/UtilizadorRegistado.java
@@ -1,30 +1,47 @@
 package org.example.model;
 
-public abstract class UtilizadorRegistado{
+import java.io.Serializable;
+
+/**
+ * Classe abstrata que representa um utilizador registado no sistema.
+ */
+public abstract class UtilizadorRegistado implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private String email;
     private String nome;
     private String password;
 
+    /**
+     * Constrói um UtilizadorRegistado com email, nome e password.
+     * @param email    email único do utilizador
+     * @param nome     nome do utilizador
+     * @param password palavra-passe de acesso
+     */
     public UtilizadorRegistado(String email, String nome, String password) {
         this.email = email;
         this.nome = nome;
         this.password = password;
     }
 
-    public boolean temPassord(String pass){
+    public String getEmail() {
+        return email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public boolean temPassord(String pass) {
         return password.equals(pass);
+    }
+
+    public boolean temNome(String username) {
+        return username.equalsIgnoreCase(nome);
     }
 
     @Override
     public String toString() {
         return nome + " <" + email + ">";
-    }
-
-    public boolean temNome(String username) {
-        return username.equals(nome);
-    }
-
-    public String getNome() {
-        return this.nome; // Substitui pelo nome da tua variável se for diferente
     }
 }
